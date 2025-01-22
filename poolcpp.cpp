458c3ab0756cc2b12d3149f663d4eda743a7a9a1
Mhmd <Mohamed2015pre@gmail.com>
2025-01-22 11:41:21 +0300

    Added some Improvments

--- poolcpp.cpp
+++ poolcpp.cpp
@@ -2,20 +2,38 @@
 using namespace std;
 
 int main(){
-	
-	cout<<"Enter the coords for the cue ball(separted by a space x y): ";
+
+	cout << "Enter the coords for the cue ball(separted by a space x y): ";
 	int x1 , y1;
-	cin>>x1>>y1;
-		cout<<"\nEnter the coords for the ball to hit(separted by a space x y): ";
+	cin >> x1 >> y1;
+	
+	cout <<"\nEnter the coords for the ball to hit(separted by a space x y): ";
 	int x2 , y2;
-	cin>>x2>>y2;
-		cout<<"\nEnter the coords for the person(separted by a space x y): ";
+	cin >> x2 >> y2;
+	
+	cout << "\nEnter the coords for the person(separted by a space x y): ";
 	int x3 , y3;
-	cin>>x3>>y3;
-	if ((y2-y1)/(x2-x1) == (y3-y1)/(x3-x1)){
-		cout<<"\nyou are going to hit";
-	} else{
-		cout<<"good";
+	cin >> x3 >> y3;
+	
+	if ((x1 - x2) != 0 && (x1 - x3) != 0){
+		if ((y2-y1)/(x2-x1) == (y3-y1)/(x3-x1)){
+		cout << "\nYou are going to hit the person";
+		cout << "Press any key to continue...";
+		cin.get();
+		
+	} else {
+		cout << "Good";
+		cout << "Press any key to continue...";
+		cin.get();
+		
 	}
+	} else {
+		cout << "Some values are invalid (x1-x2) or (x1-x3) cannot be zero";
+		cout << "Press any key to continue...";
+
+		cin.get();
+	}
+
+	return 0;
 	
 }
